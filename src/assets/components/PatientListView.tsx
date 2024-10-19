10ef02f0f18a97076a7aea36f71c2f62c68625f9
Maximilian Rauh <rauh.maximilian@gmail.com>
2024-10-19 22:11:55 +0200

    first simple request to fhir database (react bug)
    + there is a bug due to react strict mode, it triggers the useEffect twice. To resolve just remove StrictMode from main.tsx

--- src/assets/components/PatientListView.tsx
+++ src/assets/components/PatientListView.tsx
@@ -4,15 +4,41 @@ server.
 */
 
 import { Link } from "react-router-dom";
-import { MOCKPATIENT_LIST } from "../mock/MockData";
+import { useEffect, useState } from "react";
+import { PatientService } from "../services/PatientService";
+import { Patient } from "../models/Patient";
+import { AxiosResponse } from "axios";
 
 export default function PatientListView() {
+  const patientService: PatientService = new PatientService();
+  const [patients, setPatients] = useState<Patient[]>([]);
+
+  useEffect(() => {
+    const getMockPatient = async () => {
+      const request = patientService.getPatientById("patient-1");
+
+      await request
+        .then((response: AxiosResponse) => {
+          const patient = response.data as Patient;
+          setPatients((p) => [...p, patient]);
+        })
+        .catch((err) => {
+          console.log(err.message);
+          console.log("There was an error");
+        })
+        .finally(() => {
+          console.log("DONE!");
+        });
+    };
+
+    getMockPatient();
+  }, []);
+
   return (
     <>
       <div>Patient List View</div>
-
       <ol>
-        {MOCKPATIENT_LIST.map((patient) => {
+        {patients.map((patient) => {
           const detailsRouter: string = "/details/" + patient.id;
           return (
             <li key={patient.id}>
