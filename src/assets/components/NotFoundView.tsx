605613d49752c52d0507ac9e79b03b14f18a6a8f
Maximilian Rauh <rauh.maximilian@gmail.com>
2024-10-21 17:36:14 +0200

    minor bootstrap adjustments to 404 page

--- src/assets/components/NotFoundView.tsx
+++ src/assets/components/NotFoundView.tsx
@@ -6,11 +6,11 @@ import { Link } from "react-router-dom";
 
 export default function NotFoundView() {
   return (
-    <>
-      <div>The URL you tried to access could not be found!</div>
-      <button type="button">
-        <Link to="/">Go to Patient List View</Link>
+    <div className="d-flex flex-column text-center ">
+      <h2>404 - The URL you tried to access could not be found!</h2>
+      <button type="button" className="align-self-center">
+        <Link to="/">Return to Patient List View</Link>
       </button>
-    </>
+    </div>
   );
 }
