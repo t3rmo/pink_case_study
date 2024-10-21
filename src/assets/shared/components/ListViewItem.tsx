fac544ad13685796ef551f16e18a24b247351b07
Maximilian Rauh <rauh.maximilian@gmail.com>
2024-10-21 12:50:58 +0200

    reduced listviewitem complexity

--- src/assets/shared/components/ListViewItem.tsx
+++ src/assets/shared/components/ListViewItem.tsx
@@ -8,13 +8,17 @@ interface Props {
 export default function ListViewItem({ patient }: Props) {
   const detailsRouter: string = "/details/" + patient.id;
 
+  const createFullName = () => {
+    const firstName = patient.name[0]?.given.join("");
+    const lastName = patient.name[0]?.family;
+    const fullName = firstName + " " + lastName;
+    return fullName;
+  };
+
   return (
     <tbody>
       <tr>
-        <th scope="row">{patient.id}</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
+        <th scope="row">{createFullName()}</th>
         <td>
           <Link to={detailsRouter}>Details</Link>
         </td>
